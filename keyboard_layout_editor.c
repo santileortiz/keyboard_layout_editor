7f0791244d6018be859d1985dda643945c17ba6e
Santiago León O <santileortiz@gmail.com>
2018-04-20 09:58:09 -0500

    Complete keymap metadata installation
    Even though it's not ideal for now we install custom keymap metadata by
    modifying /usr/share/X11/xkb/rules/evdev.xml. The installation function
    won't overwrite a system layout, but will update one that was previously
    installed by it.

--- keyboard_layout_editor.c
+++ keyboard_layout_editor.c
@@ -4,6 +4,8 @@
 
 #include "common.h"
 #include <libxml/parser.h>
+#include <libxml/xmlsave.h>
+#include <libxml/xmlwriter.h>
 
 static inline
 bool is_blank (char *c) {
@@ -232,34 +234,249 @@ xmlNodePtr xml_get_sibling (xmlNodePtr node, const char *sibling_name)
     return node;
 }
 
-bool xkb_keymap_info_install (struct keymap_t *keymap, const char *dest_file)
+void xml_print_subtree (xmlNodePtr node)
 {
-    bool success = true;
-    xmlDocPtr doc = xmlParseFile(dest_file);
-    if (doc == NULL) {
-        printf ("XML database parsing failed.\n");
-        // We can return without leaking memory because nothing was allocated.
-        return false;
-    }
+    xmlBufferPtr buff = xmlBufferCreate ();
+    xmlSaveCtxtPtr save_ctx = xmlSaveToBuffer (buff, "UTF-8", 0);
+    xmlSaveTree (save_ctx, node);
+    xmlSaveClose (save_ctx);
 
-    xmlNodePtr curr_node = xmlDocGetRootElement (doc);
-    curr_node = curr_node->children;
-    while (curr_node != NULL && xmlStrcmp(curr_node->name, (const xmlChar *)"layoutList") != 0) {
-        curr_node = curr_node->next;
-    }
+    printf ("%.*s\n", buff->use, buff->content);
+    xmlBufferFree (buff);
+}
 
-    xmlNodePtr curr_layout = xml_get_sibling (curr_node->children, "layout");
-    while (curr_layout != NULL) {
-        xmlNodePtr configItem = xml_get_child (curr_layout, "configItem");
-        if (configItem != NULL) {
-            xmlNodePtr name_node = xml_get_child (configItem, "name");
-            xmlChar *name = xmlNodeGetContent(name_node);
-            printf ("%s\n", (char *)name);
-            xmlFree (name);
+// Searches str for the first occurence of substr, then creates a copy of str
+// where data is inserted before the line where substr was found.
+//
+// NOTE: If substr is not found returns NULL.
+char* insert_string_before_line (mem_pool_t *pool, const char *str, const char *substr,
+                                 const char *data, size_t *len)
+{
+    char *s = strstr (str, substr);
+
+    char *retval = NULL;
+    if (s) {
+        while (*s != '\n') {
+            s--;
         }
-        curr_layout = xml_get_sibling (curr_layout->next, "layout");
+        s++;
+        string_t res = strn_new (str, s - str);
+        str_cat_c (&res, data);
+        str_cat_c (&res, s);
+        retval = pom_strndup (pool, str_data(&res), str_len (&res));
+        if (len != NULL) {
+            *len = str_len (&res);
+        }
+        str_free (&res);
+    }
+    return retval;
+}
+
+char* insert_string_after_line (mem_pool_t *pool, const char *str, const char *substr,
+                                const char *data, size_t *len)
+{
+    char *s = strstr (str, substr);
+
+    char *retval = NULL;
+    if (s) {
+        s = consume_line (s);
+        string_t res = strn_new (str, s - str);
+        str_cat_c (&res, data);
+        str_cat_c (&res, s);
+        retval = pom_strndup (pool, str_data(&res), str_len (&res));
+        if (len != NULL) {
+            *len = str_len (&res);
+        }
+        str_free (&res);
+    }
+    return retval;
+}
+
+bool xkb_keymap_info_install (struct keymap_t *keymap)
+{
+    // Currently, as far as I know, systems don't look for keymap metadata
+    // anywhere else other than /usr/share/X11/xkb/rules/evdev.xml. This
+    // function installs the metadata in keymap into this system file.
+    //
+    // The function fails if the system by default has a keymap with the same
+    // name as keymap->name. But, if there is a custom keymap with the same
+    // name, then we update the metadata.
+    //
+    // Custom keyboard metadata is added as children to the 'layoutList' xml
+    // node. To separate custom from default layouts, custom keymap info is
+    // wrapped around with comments as follows
+    //
+    //      <layoutList>
+    //        <!--CUSTOM LAYOUTS START-->
+    //        ... layout nodes for custom keymaps ...
+    //        <!--CUSTOM LAYOUTS END-->
+    //        ... default layout nodes ...
+    //      </layoutList>
+    //
+    //  Sadly, this CAN break applications that parse evdev.xml, because in xml
+    //  comments are nodes. An application that does not ignore them will crash,
+    //  if it tries to read the children of a comment, thinking it was a
+    //  'layout' node. Let's hope these applications either ignore comments, or
+    //  use something like XPath. So far everything is working fine.
+    //
+
+    bool success = true;
+    const char *db_path = "/usr/share/X11/xkb/rules/evdev.xml";
+    mem_pool_t pool = {0};
+
+    // Build a new <layout> node from keymap
+    string_t new_layout_str = {0};
+    {
+        xmlBufferPtr buff = xmlBufferCreate ();
+        xmlTextWriterPtr wrtr = xmlNewTextWriterMemory (buff, 0);
+        xmlTextWriterSetIndent (wrtr, 1);
+        xmlTextWriterSetIndentString (wrtr, (const xmlChar *)"  ");
+        xmlTextWriterStartElement (wrtr, (const xmlChar *)"layout");
+        xmlTextWriterStartElement (wrtr, (const xmlChar *)"configItem");
+        xmlTextWriterWriteElement (wrtr, (const xmlChar *)"name",
+                                   (const xmlChar *)keymap->name);
+        xmlTextWriterWriteElement (wrtr, (const xmlChar *)"shortDescription",
+                                   (const xmlChar *)keymap->short_description);
+        xmlTextWriterWriteElement (wrtr, (const xmlChar *)"description",
+                                   (const xmlChar *)keymap->description);
+        xmlTextWriterStartElement (wrtr, (const xmlChar *)"languageList");
+        int i;
+        for (i=0; i<keymap->num_languages; i++) {
+            xmlTextWriterWriteElement (wrtr, (const xmlChar *)"iso639Id",
+                                       (const xmlChar *)keymap->languages[i]);
+        }
+        xmlTextWriterEndElement (wrtr);
+        xmlTextWriterEndElement (wrtr);
+        xmlTextWriterEndElement (wrtr);
+        xmlFreeTextWriter (wrtr);
+
+        // Manually indent the resulting XML. I was unable to find a way to do this
+        // with libxml.
+        str_set(&new_layout_str, "    ");
+        char *s = pom_strndup (&pool, buff->content, buff->use);
+        char *end;
+        while (*s) {
+            end = strstr (s, "\n");
+            if (end) {
+                end++;
+                if (*end != '\0') {
+                    strn_cat_c (&new_layout_str, s, end - s);
+                    str_cat_c (&new_layout_str, "    ");
+                } else {
+                    str_cat_c (&new_layout_str, s);
+                }
+            }
+            s = end;
+        }
     }
 
+    char *res;
+    size_t res_len;
+    char *db = full_file_read (&pool, db_path);
+    if (db) {
+        char *s = strstr (db, "<!--CUSTOM LAYOUTS START-->");
+        if (s) {
+            s = strstr (s, "<!--CUSTOM LAYOUTS END-->");
+        }
+        if (s) {
+            char *default_start = consume_line(s);
+            char *default_end = strstr(default_start, "</layoutList>");
+
+            string_t default_layouts = str_new ("<layoutList>");
+            strn_cat_c (&default_layouts, default_start, default_end - default_start);
+            str_cat_c (&default_layouts, "</layoutList>");
+
+            xmlDocPtr default_xml = xmlParseMemory(str_data(&default_layouts),
+                                                   str_len(&default_layouts));
+            xmlNodePtr curr_layout = xmlDocGetRootElement (default_xml); // layoutList node
+            curr_layout = xml_get_sibling (curr_layout->children, "layout");
+            while (curr_layout != NULL && success) {
+                xmlNodePtr configItem = xml_get_child (curr_layout, "configItem");
+                if (configItem != NULL) {
+                    xmlNodePtr name_node = xml_get_child (configItem, "name");
+                    xmlChar *name = xmlNodeGetContent(name_node);
+                    if (xmlStrcmp (name, (const xmlChar *)keymap->name) == 0) {
+                        success = false;
+                    }
+                    xmlFree (name);
+                }
+                curr_layout = xml_get_sibling (curr_layout->next, "layout");
+            }
+
+            // If keymap is already a custom layout update it.
+            //
+            // Look for a <layout> node with <name> == keymap->name in the full XML
+            // database, if we find it, update it. It's guearanteed to be a  custom
+            // layout because these were the only ones ignored.
+            xmlDocPtr new_node_doc = xmlParseMemory (str_data(&new_layout_str),
+                                                     str_len(&new_layout_str));
+            xmlNodePtr new_node = xmlDocGetRootElement (new_node_doc);
+            bool updated = false;
+            xmlDocPtr out_xml = xmlParseFile (db_path);
+            curr_layout = xmlDocGetRootElement (out_xml); // xkbConfigRegistry
+            curr_layout = xml_get_sibling (curr_layout->children, "layoutList");
+            curr_layout = xml_get_sibling (curr_layout->children, "layout");
+            while (curr_layout != NULL && success && !updated) {
+                xmlNodePtr configItem = xml_get_child (curr_layout, "configItem");
+                if (configItem != NULL) {
+                    xmlNodePtr name_node = xml_get_child (configItem, "name");
+                    xmlChar *name = xmlNodeGetContent(name_node);
+                    if (xmlStrcmp (name, (const xmlChar *)keymap->name) == 0) {
+                        xmlAddPrevSibling (curr_layout, new_node);
+                        xmlUnlinkNode (curr_layout);
+                        xmlFreeNode (curr_layout);
+                        updated = true;
+                    }
+                    xmlFree (name);
+                }
+                curr_layout = xml_get_sibling (curr_layout->next, "layout");
+            }
+
+            // Generate the new XML database and write it out.
+            //
+            // If we updated an existing custom layout, then get the file from the
+            // modified xmlDoc. If nothing was updated then just insert the generated
+            // node as the last custom layout.
+            if (updated) {
+                xmlBufferPtr buff = xmlBufferCreate ();
+                xmlSaveCtxtPtr save_ctx = xmlSaveToBuffer (buff, "UTF-8", 0);
+                xmlSaveDoc (save_ctx, out_xml);
+                xmlSaveClose (save_ctx);
+
+                res = pom_strndup (&pool, buff->content, buff->use);
+                res_len = buff->use;
+                xmlBufferFree (buff);
+            } else {
+                res = insert_string_before_line (&pool, db,
+                                                 "<!--CUSTOM LAYOUTS END-->",
+                                                 str_data(&new_layout_str), &res_len);
+            }
+            xmlFreeDoc (out_xml);
+            str_free (&default_layouts);
+        } else {
+            // There are no custom layouts. Write comments for the first time
+            // with the new layout node.
+            string_t res_str = str_new ("    <!--CUSTOM LAYOUTS START-->\n");
+            str_cat_c (&res_str, "    <!--\n"
+                       "    These layouts were installed by keyboard_layout_editor, these comments\n"
+                       "    are used to keep track of them. Keep them at the beginning of <layoutList>.\n"
+                       "    -->\n");
+            str_cat (&res_str, &new_layout_str);
+            str_cat_c (&res_str, "    <!--CUSTOM LAYOUTS END-->\n");
+            res = insert_string_after_line (&pool, db, "<layoutList>",
+                                            str_data(&res_str),
+                                            &res_len);
+        }
+
+        full_file_write (res, res_len, db_path);
+
+    } else {
+        success = false;
+        printf ("Failed to load keymap info database: %s.\n", db_path);
+    }
+
+    str_free (&new_layout_str);
+    mem_pool_destroy (&pool);
     return success;
 }
 
@@ -273,5 +490,5 @@ int main (int argc, char *argv[])
     keymap.description = "US layout with Spanish characters";
     keymap.languages = (char *[]){"es", "us"};
     keymap.num_languages = 2;
-    xkb_keymap_info_install (&keymap, "/usr/share/X11/xkb/rules/evdev.xml");
+    xkb_keymap_info_install (&keymap);
 }

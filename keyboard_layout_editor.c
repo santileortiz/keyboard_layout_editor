d07add39b05baf03665b4a948ffba2888e696864
Santiago León O <santileortiz@gmail.com>
2018-04-26 23:08:53 -0500

    Add a command line interface to the keymap installation API

--- keyboard_layout_editor.c
+++ keyboard_layout_editor.c
@@ -7,7 +7,22 @@
 
 int main (int argc, char *argv[])
 {
-    xkb_keymap_uninstall ("my_layout");
-    xkb_keymap_install ("./custom_keyboard.xkb");
+    if (argc > 1) {
+        if (strcmp (argv[1], "--install") == 0) {
+            if (argc == 2) {
+                printf ("Expected a keymap file to install.\n");
+            } else {
+                xkb_keymap_install (argv[2]);
+            }
+        } else if (strcmp (argv[1], "--uninstall") == 0) {
+            if (argc == 2) {
+                printf ("Expected a keymap name to uninstall.\n");
+            } else {
+                xkb_keymap_uninstall (argv[2]);
+            }
+        } else if (strcmp (argv[1], "--uninstall-everything") == 0) {
+            xkb_keymap_uninstall_everything ();
+        }
+    }
     xmlCleanupParser();
 }

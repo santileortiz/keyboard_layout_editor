aee58d5de3b21ff2788a3be8a64781f6974fe6af
Santiago León O <santileortiz@gmail.com>
2019-10-26 17:20:54 -0500

    Don't print event data if it's a repetition of an already pressed key

--- libxkbcommon_view.c
+++ libxkbcommon_view.c
@@ -49,6 +49,9 @@ struct interactive_debug_app_t {
     int num_mods;
     const char **mod_names;
 
+    // Stores if the key at the index has been pressed before.
+    bool key_pressed[KEY_CNT];
+
     struct gsettings_layout_t original_active_layout;
 };
 
@@ -60,20 +63,34 @@ static gint on_gdk_key_event (GtkWidget *widget, GdkEventKey *event, gpointer us
     struct keyboard_view_t *kv = app->keyboard_view;
     struct xkb_state *state = kv->xkb_state;
 
+    bool was_pressed = false;
+    string_t info_str = {0};
+
     xkb_keycode_t keycode = event->hardware_keycode;
     enum xkb_state_component changed;
-    printf ("type: ");
+
+    str_cat_c (&info_str, "type: ");
     if (event->type == GDK_KEY_PRESS) {
-        printf ("KEY_PRESS\n");
+        // Detect if the key was pressed before and update the state in the
+        // pressed array.
+        if (app->key_pressed[keycode]) {
+            was_pressed = true;
+        }
+        app->key_pressed[keycode] = true;
+
+        str_cat_c (&info_str, "KEY_PRESS\n");
         changed = xkb_state_update_key(state, keycode, XKB_KEY_DOWN);
+
     } else if (event->type == GDK_KEY_RELEASE) {
-        printf ("KEY_RELEASE\n");
+        app->key_pressed[keycode] = false;
+        str_cat_c (&info_str, "KEY_RELEASE\n");
         changed = xkb_state_update_key(state, keycode, XKB_KEY_UP);
+
     } else {
         invalid_code_path;
     }
 
-    printf ("state: ");
+    str_cat_c (&info_str, "state: ");
     bool is_first = true;
     for (uint32_t mask = event->state;
          mask;
@@ -81,57 +98,65 @@ static gint on_gdk_key_event (GtkWidget *widget, GdkEventKey *event, gpointer us
         uint32_t next_bit_mask = mask & -mask;
 
         if (!is_first) {
-            printf (", ");
+            str_cat_c (&info_str, ", ");
         }
         is_first = false;
-        printf ("%s", gdk_modifier_names[bit_pos(next_bit_mask)]);
+        str_cat_printf (&info_str, "%s", gdk_modifier_names[bit_pos(next_bit_mask)]);
     }
-    printf ("\n");
+    str_cat_c (&info_str, "\n");
 
-    printf ("Changed: %x\n", changed);
+    str_cat_printf (&info_str, "Changed: %x\n", changed);
 
     xkb_keysym_t keysym = xkb_state_key_get_one_sym(state, event->hardware_keycode);
 
     char keysym_name[64];
     xkb_keysym_get_name(keysym, keysym_name, sizeof(keysym_name));
 
-    printf ("keysym: %s\n", keysym_name);
+    str_cat_printf (&info_str, "keysym: %s\n", keysym_name);
 
     int size = xkb_state_key_get_utf8(state, keycode, NULL, 0) + 1;
     if (size > 1) {
         char *buffer = malloc (size);
         xkb_state_key_get_utf8(state, keycode, buffer, size);
-        printf ("UTF-8: %s\n", buffer);
+        str_cat_printf (&info_str, "UTF-8: %s\n", buffer);
     }
 
     // Print modifier information
     {
-        printf ("Effective Mods: ");
+        str_cat_c (&info_str, "Effective Mods: ");
         for (int i=0; i<app->num_mods; i++) {
             if (xkb_state_mod_index_is_active(state, i, XKB_STATE_MODS_EFFECTIVE)) {
-                printf ("%s ", app->mod_names[i]);
+                str_cat_printf (&info_str, "%s ", app->mod_names[i]);
             }
         }
-        printf ("\n");
+        str_cat_c (&info_str, "\n");
 
-        printf ("Consumed Mods (XKB): ");
+        str_cat_c (&info_str, "Consumed Mods (XKB): ");
         for (int i=0; i<app->num_mods; i++) {
             if (xkb_state_mod_index_is_consumed2 (state, keycode, i, XKB_CONSUMED_MODE_XKB)) {
-                printf ("%s ", app->mod_names[i]);
+                str_cat_printf (&info_str, "%s ", app->mod_names[i]);
             }
         }
-        printf ("\n");
+        str_cat_c (&info_str, "\n");
 
-        printf ("Consumed Mods (GTK): ");
+        str_cat_c (&info_str, "Consumed Mods (GTK): ");
         for (int i=0; i<app->num_mods; i++) {
             if (xkb_state_mod_index_is_consumed2 (state, keycode, i, XKB_CONSUMED_MODE_GTK)) {
-                printf ("%s ", app->mod_names[i]);
+                str_cat_printf (&info_str, "%s ", app->mod_names[i]);
             }
         }
-        printf ("\n");
+        str_cat_c (&info_str, "\n");
     }
 
-    printf ("\n");
+    str_cat_c (&info_str, "\n");
+
+    // Don't print event data of key repetitions as these just clutter the
+    // output.
+    // TODO: We may want to make this optional if we get serious about
+    // supporting the repeat setting of the XKB file format.
+    if (!was_pressed) {
+        printf ("%s", str_data (&info_str));
+    }
 
     return FALSE;
 }
